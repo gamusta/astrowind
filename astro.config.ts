1eadfca8a94eae43e0d9cb9bc4a83469169f307b
Mustapha GANGA <gamusta@gmail.com>
2025-05-01 11:26:37 +0200

    update

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-
+  site: "https://astro-123.netlify.app/",
   integrations: [
     tailwind({
       applyBaseStyles: false,
